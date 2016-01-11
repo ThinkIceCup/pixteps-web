68b7253d9b84f542223f2ba3aa796b382ff6fc02
Christian Soler <twenty40@gmail.com>
2016-01-11 00:31:39 -0600

    ...

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     server: {
         port: process.env.PORT || 3000
     },
-
+    api_keys: {}
 };
--- procfile.js
+++ procfile.js
@@ -1,6 +1,2 @@
-/**
- * Created by VAULTROCK on 1/10/16.
- */
-
 web: node server.js
 
